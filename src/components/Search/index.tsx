7b3db1735cd5d2741bcf12a1bb168f382d42d444
perfilovv <russianspiritoneshotlol@gmail.com>
2024-04-16 15:23:00 +0300

    feat: add debounce for input with lodash

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,33 +1,44 @@
-import { useContext, useEffect, useRef } from 'react';
+import { useCallback, useContext, useRef, useState } from 'react';
 import { IoSearchOutline, IoCloseOutline } from 'react-icons/io5';
+import debounce from 'lodash.debounce';
 import { SearchContext } from '../../App';
 
 import styles from './Search.module.scss';
 
 const Search = () => {
-    const { searchValue, setSearchValue } = useContext(SearchContext);
+    const [value, setValue] = useState('');
+    const { setSearchValue } = useContext(SearchContext);
     const inputRef: any = useRef();
 
     const onClickClear = () => {
         setSearchValue('');
+        setValue('');
         inputRef.current.focus();
     };
 
-    useEffect(() => {
-        console.log();
-    }, []);
+    const updateSearchValue = useCallback(
+        debounce((str) => {
+            setSearchValue(str);
+        }, 250),
+        []
+    );
+
+    const onChangeInput = (event: any) => {
+        setValue(event.target.value);
+        updateSearchValue(event.target.value);
+    };
 
     return (
         <div className={styles.root}>
             <IoSearchOutline className={styles.icon} />
             <input
                 ref={inputRef}
-                value={searchValue}
-                onChange={(event) => setSearchValue(event.target.value)}
+                value={value}
+                onChange={onChangeInput}
                 className={styles.input}
                 placeholder="Поиск пиццы..."
             />
-            {searchValue && (
+            {value && (
                 <IoCloseOutline
                     onClick={onClickClear}
                     className={styles.clearIcon}
